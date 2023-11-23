80bcaddc1b959974890355b002c1bda6f12ba4bb
CMcQuay <calebimcquay@gmail.com>
2023-11-22 23:12:22 -0600

    All tasks complete

--- app/src/main/java/edu/ualr/intentsassignment/ContactFormActivity.java
+++ app/src/main/java/edu/ualr/intentsassignment/ContactFormActivity.java
@@ -1,6 +1,9 @@
 package edu.ualr.intentsassignment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +15,22 @@ public class ContactFormActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_form);
+        Button saveContactButton = findViewById(R.id.saveBtn);
+        saveContactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get contact information from the form
+                String contactName = "John Doe"; // Replace with actual data
+                String contactEmail = "john.doe@example.com"; // Replace with actual data
+
+                // Start ContactInfoActivity and pass data
+                Intent intent = new Intent(ContactFormActivity.this, ContactInfoActivity.class);
+                intent.putExtra("contact_name", contactName);
+                intent.putExtra("contact_email", contactEmail);
+                // Add more data as needed
+                startActivity(intent);
+            }
+        });
     }
     // TODO 06. Create a new method that reads the values in the several EditText elements of the layout and then uses the Contact class to send those data to ContactInfoActivity
 }
--- app/src/main/java/edu/ualr/intentsassignment/ContactInfoActivity.java
+++ app/src/main/java/edu/ualr/intentsassignment/ContactInfoActivity.java
@@ -1,10 +1,35 @@
 package edu.ualr.intentsassignment;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ContactInfoActivity extends AppCompatActivity {
     // TODO 03. Create a new layout file to define the GUI elements of the ContactInfoActivity.
     // TODO 04. Define the basic skeleton of the ContactInfoActivity. Inflate the layout defined in the first step to display the GUI elements on screen.
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_info);
+
+        // Retrieve contact information from the intent
+        Intent intent = getIntent();
+        String contactName = intent.getStringExtra("contact_name");
+        String contactEmail = intent.getStringExtra("contact_email");
+        // Add more fields as needed
+
+        // Update the UI with the contact information
+        TextView nameTextView = findViewById(R.id.textViewName);
+        TextView emailTextView = findViewById(R.id.textViewEmail);
+        // Reference other TextViews as needed
+
+        nameTextView.setText("Name: " + contactName);
+        emailTextView.setText("Email: " + contactEmail);
+        // Set other TextViews accordingly
+    }
     // TODO 07. Create a new method that reads the contact info coming from ContactFormActivity and use it to populate the several TextView elements in the layout.
     // TODO 08. Create a new method that invokes a Phone Dialer app, using as parameter the phone number included in the contact info received from ContactFormActivity in the previous step
     // TODO 09. Create a new method that invokes a Messages app, using as parameter the phone number included in the contact info received from ContactFormActivity in the 7th step
